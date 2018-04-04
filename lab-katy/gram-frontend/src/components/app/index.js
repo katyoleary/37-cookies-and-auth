59eea488e49eeac06e124db52b3f92f5966023d9
Katy Robinson <katyorob@gmail.com>
2018-04-04 16:02:45 -0700

    app component updated with new containers

--- lab-katy/gram-frontend/src/components/app/index.js
+++ lab-katy/gram-frontend/src/components/app/index.js
@@ -5,10 +5,22 @@ import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import appCreateStore from '../../lib/app-create-store';
 import Homepage from '../homepage';
+import SettingsContainer from '../settings';
+import * as util from '../../lib/util';
+import { tokenSet } from '../../actions/authentication-actions';
 
 let store = appCreateStore();
 
+
 class App extends React.Component {
+
+  componentDidMount() {
+    let token = util.readCookie('X-Sluggram-Token');
+    if(token) {
+      this.props.tokenSet(token);
+    }
+  }
+
   render() {
     return (
       <section className='gram'>
@@ -21,10 +33,12 @@ class App extends React.Component {
                     <ul>
                       <li><Link to='/welcome/signup'>signup</Link></li>
                       <li><Link to='/welcome/login'>login</Link></li>
+                      <li><Link to='/settings'>settings</Link></li>
                     </ul>
                   </nav>
               </header>
               <Route path='/welcome/:authentication' component={Homepage} />
+              <Route exact path='/settings' component={SettingsContainer} />
             </section>
           </BrowserRouter>
         </Provider>
