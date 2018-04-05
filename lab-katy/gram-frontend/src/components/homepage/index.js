da38528558f42ad7c3f79862933c24d06edee41c
Katy Robinson <katyorob@gmail.com>
2018-04-05 16:44:38 -0700

    refactored homepage

--- lab-katy/gram-frontend/src/components/homepage/index.js
+++ lab-katy/gram-frontend/src/components/homepage/index.js
@@ -13,9 +13,35 @@ class Homepage extends React.Component {
   constructor(props) {
     super(props);
 
-    
+    this.handleLogin = this.handleLogin.bind(this)
+    this.handleSignup = this.handleSignup.bind(this)
   }
 
+  componentWillReceiveProps(props) {
+    if(props.auth && props.userProfile) {
+      props.history.replace('/dashboard')
+    }
+
+    if(props.auth && !props.userProfile) {
+      props.history.replace('/settings')
+    }
+  };
+
+  handleLogin(user) {
+    let { profileFetch, history } = this.props;
+    return this.props.login(user)
+    .then( () => profileFetch())
+    .then( () => history.push('/dashboard'))
+    .catch(util.logError);
+  };
+
+  handleSignup(user) {
+    return this.props.signup(user)
+    .then( () => {
+      this.props.history.push('/settings')
+    })
+    .catch(util.logError);
+  };
 
   render() {
     let { params } = this.props.match;
@@ -25,18 +51,32 @@ class Homepage extends React.Component {
     //authentication coming from our route. im app <Route path='welcome/:authentication'>
 
     return (
-      <section>
+      <section className='homepage'>
         <AuthenticationForm authentication={params.authentication} onComplete={handleComplete} />
+
+        <div className='auth-nav'>
+          {util.renderIf(params.authentication === 'login', 
+          <Link to='/welcome/signup'>signup</Link>)}
+
+          {util.renderIf(params.authentication === 'signup', 
+            <Link to='/welcome/login'>login</Link>)}
+        </div>
       </section>
     )
   }
 }
 
+const mapStateToProps = state => ({
+  auth: state.props,
+  userProfile: state.userProfile
+});
+
 const mapDispatchToProps = dispatch => {
   return {
     signup: user => dispatch(signupRequest(user)), 
     login: user => dispatch(loginRequest(user)),
+    profileFetch: () => dispatch(userProfileFetchRequest())
   }
-}
+};
 
-export default connect(null, mapDispatchToProps)(Homepage);
+export default connect(mapStateToProps, mapDispatchToProps)(Homepage);
